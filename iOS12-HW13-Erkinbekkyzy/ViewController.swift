5acc90261fdaff4b239827e2b385420c4dec25ef
bemadeveloper <akbermet1990@icloud.com>
2024-02-26 21:18:57 +0600

    Initial Commit

---
+++ iOS12-HW13-Erkinbekkyzy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW13-Erkinbekkyzy
+//
+//  Created by Bema on 26/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
